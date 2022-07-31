f374f99b6ab3bcc08bdeab660cc0c66999b9eaa1
Ademola <ademolaogunmokun@yahoo.com>
2022-07-31 15:55:55 +0100

    spread

--- src/Components/CreateNew.jsx
+++ src/Components/CreateNew.jsx
@@ -5,19 +5,21 @@ import {useField} from '../hooks/index'
 
 const CreateNew = ({addNew}) => {
   const content = useField('text')
+  const author  = useField('text')
+  const info = useField('text')
+  const votes = useField('text')
   const navigate = useNavigate()
   
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
+
 
 
   const handleSubmit = (e) => {
     e.preventDefault()
     addNew({
       content: content.value,
-      author,
-      info,
-      votes: 0
+      author: author.value,
+      info : info.value,
+      votes: votes.value
     })
 
     navigate('/')
@@ -29,17 +31,17 @@ const CreateNew = ({addNew}) => {
       <form onSubmit={handleSubmit}>
         <div>
            <label htmlFor="content"> content
-           <input type={content.type} name='content' value={content.value} onChange={content.onChange}/>
+           <input {...content}/>
            </label>
           
         </div>
         <div>
           author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+          <input {...author} />
         </div>
         <div>
           url for more info
-          <input name='info' value={info} onChange={(e)=> setInfo(e.target.value)} />
+          <input {...info} />
         </div>
         <button>create</button>
       </form>
