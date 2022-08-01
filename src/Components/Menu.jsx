4ab2af7d9472d10bfb3624203fec054bce684856
Ademola <ademolaogunmokun@yahoo.com>
2022-08-01 08:04:26 +0100

    Menu

--- src/Components/Menu.jsx
+++ src/Components/Menu.jsx
@@ -3,11 +3,6 @@ const Menu = () => {
     paddingRight: 5
   }
   return (
-    // <div>
-    //   <a href='#' style={padding}>anecdotes</a>
-    //   <a href='#' style={padding}>create new</a>
-    //   <a href='#' style={padding}>about</a>
-    // </div>
 
     <div>
 
